08cab56139c0c9267aa06950c0de77a4cd4569ae
Eva Veshtort <eva.veshtort@gmail.com>
2024-10-13 17:22:32 +0300

    old project deleted

--- stat_calc_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import CalcMetrics, Calculations, Metrics
-
-admin.site.register(CalcMetrics)
-admin.site.register(Calculations)
-admin.site.register(Metrics)
--- stat_calc_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StatCalcAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "stat_calc_app"
